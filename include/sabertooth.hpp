086421648fae0d8473d2311a74358884bbf3d044
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-11-08 02:25:29 +0530

    Sabertooth: Unify cpp and hpp
    Template definitions and declarations should not be separated in the
    first place.

--- include/sabertooth.hpp
+++ include/sabertooth.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <algorithm>
+#include <numeric>
+#include <stdexcept>
 #include <vector>
 
 namespace rml {
@@ -21,9 +24,19 @@ public:
   /// integer between 0 and 255)
   /// @returns The raw byte values representing the command, which can be
   /// directly sent to the driver.
-  std::vector<unsigned char> const& construct_command(unsigned char addr,
+  std::vector<unsigned char> const& construct_command([[maybe_unused]] unsigned char addr,
                                                       unsigned char motor_index,
-                                                      int motor_spd);
+                                                      int motor_spd)
+  {
+
+    if (motor_index == 0)
+      last_cmd[0] = std::clamp(int(motor_spd / 2), 1, 127);
+    else if (motor_index == 1)
+      last_cmd[1] = std::clamp(int(motor_spd / 2)+128, 128, 255);
+    else
+      throw std::runtime_error("Cannot command motor at out of range index");
+    return last_cmd;
+  }
   SimplifiedSerialConfig()
     : last_cmd(2)
   {}
@@ -53,13 +66,34 @@ public:
   /// directly sent to the driver.
   std::vector<unsigned char> const& construct_command(unsigned char addr,
                                                       unsigned char motor_index,
-                                                      int motor_spd);
+                                                      int motor_spd)
+  {
+    last_cmd[0] = addr;
+    last_cmd[1] = cmd_[motor_index][motor_spd > 128];
+    last_cmd[2] = motor_spd / 2;
+    last_cmd[3] =
+      std::accumulate(last_cmd.begin(), last_cmd.end() - 1, 0) & 0x7F;
+    return last_cmd;
+  }
   /// Construct a greeting message which is used to identify baudrate being
   /// used. All commands sent before a greet are ignored.
   /// @returns The raw byte values representing the command, which can be
   /// directly sent to the driver.
-  std::vector<unsigned char> const& greet();
-  PacketizedSerialConfig();
+  std::vector<unsigned char> const& greet()
+  {
+    std::fill(last_cmd.begin(), last_cmd.end(), 0);
+    last_cmd[3] = GREET_CODE;
+    return last_cmd;
+  }
+  PacketizedSerialConfig()
+    : last_cmd(4)
+  {
+
+    cmd_[0][0] = 0x01; // Drive motor 1 backwards
+    cmd_[0][1] = 0x00; // Drive motor 1 forwards
+    cmd_[1][0] = 0x04; // Drive motor 2 backwards
+    cmd_[1][1] = 0x05; // Drive motor 2 forwards
+  }
 
 private:
   unsigned char cmd_[2][2];
@@ -77,7 +111,11 @@ public:
   /// @param speed The speed in range [-1,1] both inclusive.
   /// @returns The intermediate representation of given speed after
   /// interpolation.
-  int transform(float speed);
+  int transform(float speed)
+  {
+
+    return std::clamp(int(speed * 127 + 128), 1, 255);
+  }
 
 protected:
   ~LinearInterpolation() {}
@@ -129,8 +167,17 @@ public:
   /// @returns Returned data from OutputInterface::send(), exactly.
   typename OutputInterface::ReturnType command(unsigned char addr,
                                                unsigned char motor_index,
-                                               float motor_spd);
-  typename OutputInterface::ReturnType command_send();
+                                               float motor_spd)
+  {
+    int interm_rep_spd = Interpolation::transform(motor_spd);
+    auto cmd =
+      MotorConfig::construct_command(addr, motor_index, interm_rep_spd);
+    return OutputInterface::send(cmd);
+  }
+  typename OutputInterface::ReturnType command_send()
+  {
+    return OutputInterface::send(MotorConfig::last_cmd);
+  }
 };
 typedef Sabertooth<SimplifiedSerialConfig, LinearInterpolation, SerialOutput>
   BasicSabertooth;
