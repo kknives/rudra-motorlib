d1ff7f4501dd43791f2aa296af38020b2caf97a9
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-11-08 22:42:18 +0530

    Sabertooth: Add correlation tests

--- tests/test_sabertooth.cpp
+++ tests/test_sabertooth.cpp
@@ -1,41 +1,85 @@
 #include "sabertooth.hpp"
 #include <gtest/gtest.h>
 
-      TEST(SabertoothTest, MaxMotorSpeed) {
-      auto s = rml::Sabertooth<rml::SimplifiedSerialConfig, rml::LinearInterpolation, rml::ReturnOutput>();
-        auto vel = s.command(0,0,1.0f);
-        vel = s.command(0, 1,1.0f);
-        EXPECT_EQ(vel[0], 127);
-        EXPECT_EQ(vel[1], 255);
-      }
-      TEST(SabertoothTest, BoundaryInvariants) {
-      auto s = rml::Sabertooth<rml::SimplifiedSerialConfig, rml::LinearInterpolation, rml::ReturnOutput>();
-        for (int i = -100; i <= 100; i++) {
-          auto vel = s.command(0, 0, (float)i/100);
-          vel = s.command(0, 1, (float)i/100);
-          EXPECT_LT(vel[0], 128);
-          EXPECT_GT(vel[0], 0);
-          EXPECT_LT(vel[1], 256);
-          EXPECT_GT(vel[1], 127);
-        }
-      }
-  // rc::check("[Sabertooth] Check max motor speed", [&s] () {
-  //     const auto full_spd = 1;
-  //     RC_ASSERT(s.construct_command(0, 255)[0] == 127);
-  //     RC_ASSERT(s.construct_command(0, full_spd)[1] == 255);
-  //     });
-  // rc::check("[Sabertooth] Command invariants", [&s]() {
-  //     const auto motor1_spd = rc::gen::inRange<float>(0,2);
-  //     const auto motor2_spd = rc::gen::inRange<float>(0,2);
-  //     auto vel = s.command(0, *motor1_spd-1);
-  //     vel = s.command(1, *motor2_spd-1);
-  //     RC_ASSERT(vel[0] < 128 && vel[0] > 0);
-  //     RC_ASSERT(vel[1] < 256 && vel[1] > 127);
-  //     });
-  // rc::check("[Sabertooth] Linear command correlation", [&s]() {
-  //     const auto motor1_spd = rc::gen::inRange<float>(0,2);
-  //     const auto motor2_spd = rc::gen::inRange<float>(0,2);
-  //     auto vel = s.command(0, *motor1_spd-1);
-  //     vel = s.command(1, *motor2_spd-1);
-  //     RC_ASSERT(((vel[0]-64)/127) == *motor1_spd);
-  //     });
+TEST(SabertoothTest, EndpointSpeeds)
+{
+  auto s = rml::Sabertooth<rml::SimplifiedSerialConfig,
+                           rml::LinearInterpolation,
+                           rml::ReturnOutput>();
+  auto vel = s.command(0, 0, 1.0f);
+  vel = s.command(0, 1, 1.0f);
+  EXPECT_EQ(vel[0], 127);
+  EXPECT_EQ(vel[1], 255);
+  vel = s.command(0, 0, 0.0f);
+  vel = s.command(0, 1, 0.0f);
+  EXPECT_EQ(vel[0], 64);
+  EXPECT_EQ(vel[1], 192);
+}
+TEST(SabertoothTest, BoundaryInvariants)
+{
+  auto s = rml::Sabertooth<rml::SimplifiedSerialConfig,
+                           rml::LinearInterpolation,
+                           rml::ReturnOutput>();
+  for (int i = -100; i <= 100; i++) {
+    auto vel = s.command(0, 0, (float)i / 100);
+    vel = s.command(0, 1, (float)i / 100);
+    EXPECT_LT(vel[0], 128);
+    EXPECT_GT(vel[0], 0);
+    EXPECT_LT(vel[1], 256);
+    EXPECT_GT(vel[1], 127);
+  }
+}
+TEST(SabertoothTest, NegativeCorrelation)
+{
+  auto s = rml::Sabertooth<rml::SimplifiedSerialConfig,
+                           rml::LinearInterpolation,
+                           rml::ReturnOutput>();
+  for (int i = -100; i < 0; i++) {
+    auto vel = s.command(0, 0, (float)i / 100);
+    vel = s.command(0, 1, (float)i / 100);
+    EXPECT_LT(vel[0], 64);
+    EXPECT_GT(vel[0], 0);
+    EXPECT_LT(vel[1], 192);
+    EXPECT_GT(vel[1], 127);
+  }
+}
+TEST(SabertoothTest, PositiveCorrelation)
+{
+  auto s = rml::Sabertooth<rml::SimplifiedSerialConfig,
+                           rml::LinearInterpolation,
+                           rml::ReturnOutput>();
+  for (int i = 1; i < 100; i++) {
+    auto vel = s.command(0, 0, (float)i / 100);
+    vel = s.command(0, 1, (float)i / 100);
+    EXPECT_LT(vel[0], 128);
+    EXPECT_GT(vel[0], 63);
+    EXPECT_LT(vel[1], 256);
+    EXPECT_GT(vel[1], 191);
+  }
+}
+TEST(SabertoothTest, IncrementalCorrelation)
+{
+  auto s = rml::Sabertooth<rml::SimplifiedSerialConfig,
+                           rml::LinearInterpolation,
+                           rml::ReturnOutput>();
+  auto vel = s.command(0, 0, 0);
+  vel = s.command(0, 0, 0);
+  for (int i = 1; i < 100; i++) {
+    vel = s.command(0, 0, (float)i / 100);
+    EXPECT_GT(vel[0]-63, vel[1]-192);
+    vel = s.command(0, 1, (float)i / 100);
+  }
+}
+TEST(SabertoothTest, DecrementalCorrelation)
+{
+  auto s = rml::Sabertooth<rml::SimplifiedSerialConfig,
+                           rml::LinearInterpolation,
+                           rml::ReturnOutput>();
+  auto vel = s.command(0, 0, 0);
+  vel = s.command(0, 1, 0);
+  for (int i = 1; i < 100; i++) {
+    vel = s.command(0, 0, (float)i / 100);
+    EXPECT_LT(vel[0]-64, vel[1]-190);
+    vel = s.command(0, 1, (float)i / 100);
+  }
+}
