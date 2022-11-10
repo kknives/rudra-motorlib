4b0a0b37a048069aeb3162e1ef8a57a42d139af5
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-11-11 00:32:56 +0530

    SmartElex: Implement Boundary Checks

--- tests/test_smartelex.cpp
+++ tests/test_smartelex.cpp
@@ -31,16 +31,16 @@ TEST(SmartElexTest, EndpointSpeeds)
   EXPECT_EQ((vel[1])& 0b11, 3);
   EXPECT_GT(vel[3], 200);
 }
-// TEST(SmartElexTest, BoundaryInvariants)
-// {
-//   auto s = rml::SmartElex<rml::SerialModeConfig,
-//                            rml::LinearInterpolation,
-//                            rml::ReturnOutput>();
-//   for (int i = -100; i <= 100; i++) {
-//     auto vel = s.command(0, float(i)/100, (float)i / 100);
-//     EXPECT_LT(vel[2], 256);
-//     EXPECT_GE(vel[2], 0);
-//     EXPECT_LT(vel[3], 256);
-//     EXPECT_GE(vel[3], 0);
-//   }
-// }
+TEST(SmartElexTest, BoundaryInvariants)
+{
+  auto s = rml::SmartElex<rml::SerialModeConfig,
+                           rml::LinearInterpolation,
+                           rml::ReturnOutput>();
+  for (int i = -100; i <= 100; i++) {
+    auto vel = s.command_multiple(float(i)/100, (float)i / 100);
+    EXPECT_LT(vel[2], 256);
+    EXPECT_GE(vel[2], 0);
+    EXPECT_LT(vel[3], 256);
+    EXPECT_GE(vel[3], 0);
+  }
+}
