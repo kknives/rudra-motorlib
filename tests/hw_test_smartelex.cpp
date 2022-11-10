5e333013210e9e2b933de39fab6cce8861a1bf2c
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-11-10 16:49:06 +0530

    SmartElex: Binary for hardware tests

--- tests/hw_test_smartelex.cpp
+++ tests/hw_test_smartelex.cpp
@@ -4,8 +4,8 @@ int main() {
   auto s = rml::SmartElex<rml::SerialModeConfig,
                            rml::LinearInterpolation,
                            rml::SerialOutput>();
-  s.open("/dev/pts/4");
-  s.command(1.0f, 1.0f);
+  s.open("/dev/ttyUSB0");
+  s.command_multiple(0.0f, 0.0f);
   printf("%d %d %d %d %d\n",s.last_cmd[0],s.last_cmd[1],s.last_cmd[2],s.last_cmd[3],s.last_cmd[4]);
   return 0;
 }
