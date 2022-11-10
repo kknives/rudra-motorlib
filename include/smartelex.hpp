dc6d751af234da3f1a54ad2f5ef9d237a9e0f889
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-11-10 16:34:23 +0530

    SmartElex: Uniform Interface for `command`
    ~ Added `usigned char addr` to `SerialOutput::construct_command`
    + `SerialOutput::construct_multiple`
    ~ Added `usigned char addr` to `SmartElex::command`
    + `SmartElex::command_multiple`

--- include/smartelex.hpp
+++ include/smartelex.hpp
@@ -5,8 +5,11 @@ class SerialModeConfig
 {
 public:
   std::vector<unsigned char> last_cmd;
-  std::vector<unsigned char> const& construct_command(int motor1_spd,
-                                                      int motor2_spd);
+  std::vector<unsigned char> const& construct_command(unsigned char addr,
+                                                      unsigned char motor_index,
+                                                      int motor_spd);
+  std::vector<unsigned char> const& construct_multiple(int motor1_spd,
+                                                       int motor2_spd);
   SerialModeConfig();
 
 protected:
@@ -20,16 +23,20 @@ class SmartElex
 {
 public:
   SmartElex(){};
-  typename OutputInterface::ReturnType command(
-    [[maybe_unused]] unsigned char addr,
-    unsigned char motor_index,
-    float motor_spd);
-  typename OutputInterface::ReturnType command(float motor1_spd,
-                                               float motor2_spd)
+  typename OutputInterface::ReturnType command(unsigned char addr,
+                                               unsigned char motor_index,
+                                               float motor_spd)
+  {
+    int interm_spd = Interpolation::transform(motor_spd);
+    auto cmd = MotorConfig::construct_command(addr, motor_index, interm_spd);
+    return OutputInterface::send(cmd);
+  }
+  typename OutputInterface::ReturnType command_multiple(float motor1_spd,
+                                                        float motor2_spd)
   {
     int interm_spd1 = Interpolation::transform(motor1_spd);
     int interm_spd2 = Interpolation::transform(motor2_spd);
-    auto cmd = MotorConfig::construct_command(interm_spd1, interm_spd2);
+    auto cmd = MotorConfig::construct_multiple(interm_spd1, interm_spd2);
     return OutputInterface::send(cmd);
   }
 };
--- lib/smartelex.cpp
+++ lib/smartelex.cpp
@@ -1,17 +1,40 @@
 #include "smartelex.hpp"
 #include <cmath>
+#include <stdexcept>
 
 namespace rml {
 std::vector<unsigned char> const&
-SerialModeConfig::construct_command(int motor1_spd, int motor2_spd)
+SerialModeConfig::construct_multiple(int motor1_spd, int motor2_spd)
 {
   last_cmd[0] = 0x2A;
   last_cmd[1] = ((motor1_spd != 128) << 3) | ((motor1_spd > 128) << 2) |
                 ((motor2_spd != 128) << 1) | (motor2_spd > 128);
-  last_cmd[2] = std::abs(motor1_spd - 128);
-  last_cmd[3] = std::abs(motor2_spd - 128);
+  last_cmd[2] = 2*std::abs(motor1_spd - 128);
+  last_cmd[3] = 2*std::abs(motor2_spd - 128);
   last_cmd[4] = 0x23;
   return last_cmd;
+}
+std::vector<unsigned char> const&
+SerialModeConfig::construct_command(unsigned char,
+                                    unsigned char motor_index,
+                                    int motor_spd)
+{
+  if (motor_index == 0) {
+    last_cmd[0] = 0x2A;
+    last_cmd[1] =
+      (last_cmd[1]&0x03) | (((motor_spd != 128) << 3) | ((motor_spd > 128) << 2));
+    last_cmd[2] = 2*std::abs(motor_spd - 128);
+    last_cmd[4] = 0x23;
+    return last_cmd;
+  } else if (motor_index == 1) {
+    last_cmd[0] = 0x2A;
+    last_cmd[1] = (last_cmd[1]&0x0C) | (((motor_spd != 128) << 1) | (motor_spd > 128));
+    last_cmd[3] = 2*std::abs(motor_spd - 128);
+    last_cmd[4] = 0x23;
+    return last_cmd;
+  } else {
+    throw std::runtime_error("Cannot command motor at out of range index");
+  }
 }
 SerialModeConfig::SerialModeConfig()
   : last_cmd(5)
